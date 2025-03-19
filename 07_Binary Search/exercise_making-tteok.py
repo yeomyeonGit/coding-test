cf7490bcd00c45431a687bb92e4c4f598d5daf77
yeomyeonGit <yeomyeongforbusiness@gmail.com>
2025-03-19 22:29:57 +0900

    250319

--- 07_Binary Search/exercise_making-tteok.py
+++ 07_Binary Search/exercise_making-tteok.py
@@ -2,23 +2,26 @@ n, m = map(int, input().split())
 lst = list(map(int, input().split()))
 lst.sort()
 
-
-total = 0
 start = 0
 end = max(lst)
 
+result = 0
+
 while start <= end:
-    mid = (0 + end) // 2
+    total = 0
+    mid = (start + end) // 2
     for x in lst:
         if x > mid:
             total += x - mid
 
-    if total > m:
-        start = mid + 1
-    elif total < m:
-        start = mid + 1
+    if total < m:
+        end = mid - 1
+    # elif total > m:
+    #     start = mid + 1
     else:
         result = mid
+        start = mid + 1
+
 
 print(result)
 
