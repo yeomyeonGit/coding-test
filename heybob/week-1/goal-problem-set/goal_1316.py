0b4463990112e9d8ba04c57ddc22541dcc7cede1
yeomyeonGit <yeomyeongforbusiness@gmail.com>
2025-03-30 22:58:22 +0900

    250330

--- heybob/week-1/goal-problem-set/goal_1316.py
+++ heybob/week-1/goal-problem-set/goal_1316.py
@@ -2,11 +2,11 @@
 # 모르겠음
 
 # break를 깜빡
+# isGroup 플래그
 
 
-import sys
-
-n = int(input())
+# 1회
+"""n = int(input())
 lst = [input() for _ in range(n)]
 number = 0
 
@@ -19,28 +19,38 @@ for word in lst:
         if prev != word[j]:
             arr.append(prev)
             prev = word[j]
-            
+
         if word[j] in arr:
             isGroup = False
             break
 
     if isGroup:
         number += 1
-    
+
     # print(word, j, isGroup )
 
+print(number)"""
+
+# 2회
+n = int(input())
+words = [input() for _ in range(n)]
+number = 0
+
+for word in words:
+    prev = word[0]
+    before = []
+    isGroup = True
+
+    for i in range(len(word)):
+        if word[i] != prev:
+            if word[i] in before:
+                isGroup = False
+                break
+            else:
+                before.append(prev)
+                prev = word[i]
+
+    if isGroup:
+        number += 1
+
 print(number)
-
-            
-            
-        #     if lst[i][j] in arr:
-        #     continue
-        # else:
-
-            # prev = lst[i][j]
-            # arr.append(lst[i][j])
-             
-        
-    
-
-
