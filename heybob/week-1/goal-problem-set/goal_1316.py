28a024fdd424c20da1b2ac89e8571d95d50f8cee
yeomyeonGit <yeomyeongforbusiness@gmail.com>
2025-03-30 22:25:16 +0900

    250330

--- heybob/week-1/goal-problem-set/goal_1316.py
+++ heybob/week-1/goal-problem-set/goal_1316.py
@@ -1,11 +1,46 @@
 # https://www.acmicpc.net/problem/1316
 # 모르겠음
 
+# break를 깜빡
+
 
 import sys
 
 n = int(input())
-print(n)
-
 lst = [input() for _ in range(n)]
-print(lst)
+number = 0
+
+for word in lst:
+    prev = word[0]
+    arr = []
+    isGroup = True
+
+    for j in range(len(word)):
+        if prev != word[j]:
+            arr.append(prev)
+            prev = word[j]
+            
+        if word[j] in arr:
+            isGroup = False
+            break
+
+    if isGroup:
+        number += 1
+    
+    # print(word, j, isGroup )
+
+print(number)
+
+            
+            
+        #     if lst[i][j] in arr:
+        #     continue
+        # else:
+
+            # prev = lst[i][j]
+            # arr.append(lst[i][j])
+             
+        
+    
+
+
